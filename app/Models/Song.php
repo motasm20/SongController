f625e4d2d0fd01ce869aec7d9e78d20b97822920
motasm <ps171179@summacollege.nl>
2024-09-26 10:05:09 +0200

    erorr's gefixt

--- app/Models/Song.php
+++ app/Models/Song.php
@@ -7,17 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Song extends Model
 {
-<<<<<<< HEAD
     use HasFactory;
     
-=======
-    use HasFactory; // Toegevoegd voor model factories
-
-    protected $table = 'songs';
-    protected $primaryKey = 'id';
-    protected $keyType = 'int';
-    public $timestamps = true;
-
->>>>>>> 9b4f50ddc632997303448288f9b737cbc01901ac
     protected $fillable = ['title', 'singer'];
 }
